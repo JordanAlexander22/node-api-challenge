d6c87d7b567996747d3231e6cabc8ddd6268b045
jordan <jordan@jordans-MacBook-Air.local>
2020-01-17 15:35:51 +0700

    testing endpoint for project route

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const server = express();
-//const projectRouter= require("./routes/projectRoute");
+const projectRouter= require("./routes/projectRoute");
 //const actionRouter= require("./routes/actionRoute");
 
 server.use(express.json());
 
+server.use('/projects', projectRouter);
 
 
 server.get("/", (req, res) => {
