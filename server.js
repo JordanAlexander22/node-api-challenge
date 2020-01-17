c4078c3ebf11db3a67a813afe143a07245c1ad3e
jordan <jordan@jordans-MacBook-Air.local>
2020-01-17 15:19:21 +0700

    basic project setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const server = express();
+//const projectRouter= require("./routes/projectRoute");
+//const actionRouter= require("./routes/actionRoute");
+
+server.use(express.json());
+
+
+
+server.get("/", (req, res) => {
+    res.send(`<h1> testing testing...</h1>`);
+  });
+  
+  
+  module.exports = server;
